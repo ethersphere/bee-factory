b49e96c962b372ca5a7d18145a412e20d6fb97ac
nugaon <toth.viktor.levente@gmail.com>
2021-04-20 16:20:49 +0200

    fix: change auto-mining to progressing blockchain in order to satisfy bee ts check

--- scripts/blockchain.sh
+++ scripts/blockchain.sh
@@ -9,6 +9,7 @@ if [ -z "$CONTAINER_IN_DOCKER" ]; then
     --name swarm-test-blockchain -d \
     trufflesuite/ganache-cli ganache-cli \
       -d -i 4020 -h 0.0.0.0 -p 9545 \
+      -b 1 \ # necessary because anyway the Bee throws Error: waiting backend sync: Post "http://swarm-test-blockchain:9545": EOF 
       --db swarm-testchain --gasLimit 6721975
 else
   docker start $NAME
