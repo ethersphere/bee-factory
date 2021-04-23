29639f92867ee97834e114eccb0b09b7e65497ff
nugaon <toth.viktor.levente@gmail.com>
2021-04-23 18:28:09 +0200

    feat: remove Dockerfile creation and simplify the script

--- scripts/bee-docker-build.sh
+++ scripts/bee-docker-build.sh
@@ -11,9 +11,8 @@ DOCKERFILE
 
 dockerbuild() {
   IMAGE_NAME=`basename $1`
-  docker build $1 -t ethersphere/$IMAGE_NAME:$2
+  docker build $1 --no-cache -f "$2" -t ethersphere/$IMAGE_NAME:$3
 }
-
 
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
@@ -22,18 +21,11 @@ BEE_VERSION=0.5.3
 # Make sure we the user has permission all the files
 sudo chmod 777 -R "$MY_PATH/bee-data-dirs"
 
-echo "Init Dockerfile for Bee nodes"
-for BEE_DIR in $BEE_DIRS
-do
-  echo "$BEE_DIR"
-  dockerfile "$MY_PATH/bee-data-dirs/$BEE_DIR/Dockerfile" $BEE_VERSION
-done
-
 echo "Build Dockerfiles"
 for BEE_DIR in $BEE_DIRS
 do
   echo "$BEE_DIR"
-  dockerbuild "$MY_PATH/bee-data-dirs/$BEE_DIR" "$BEE_VERSION"
+  dockerbuild "$MY_PATH/bee-data-dirs/$BEE_DIR" "$MY_PATH/bee-data-dirs/Dockerfile" "$BEE_VERSION"
 done
 
 echo "Docker image builds were successful!"
