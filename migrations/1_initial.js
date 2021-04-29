1754c29a2af2c513dd97272d0106d3886ccb9670
nugaon <toth.viktor.levente@gmail.com>
2021-04-29 18:16:03 +0200

    feat: postage stamp

--- migrations/1_initial.js
+++ migrations/1_initial.js
@@ -2,34 +2,71 @@ const ERC20PresetMinterPauser = artifacts.require("ERC20PresetMinterPauser");
 const FS = require('fs')
 const Path = require('path')
 
-function getSimpleSwapFactoryBin(tokenAddress) {
-  const simpleSwapFactoryBinPath = Path.join(__dirname, '..', 'contracts', 'SimpleSwapFactory.bytecode')
-  const baseBin = FS.readFileSync(simpleSwapFactoryBinPath, 'utf8').toString()
-  //remove 0x prefix
-  tokenAddress = tokenAddress.substring(2).toLowerCase().padStart(64, '0')
-  //add tokenaddress for param to the end of the bytecode
-  return baseBin + tokenAddress
+function prefixedAddressParamToByteCode(address) {
+  // the first 2 chars removal removes 0x prefix
+  return address.substring(2).toLowerCase().padStart(64, '0')
 }
 
-async function createSimpleSwapFactoryContract(erc20ContractAddress, creatorAccount) {
+function getSimpleSwapFactoryBin(tokenAddress) {
+  const binPath = Path.join(__dirname, '..', 'contracts', 'SimpleSwapFactory.bytecode')
+  const bin = FS.readFileSync(binPath, 'utf8').toString()
+  tokenAddress = prefixedAddressParamToByteCode(tokenAddress)
+  //add tokenaddress for param to the end of the bytecode
+  return bin + tokenAddress
+}
+
+function getPostageStampBin(tokenAddress) {
+  const binPath = Path.join(__dirname, '..', 'contracts', 'PostageStamp.bytecode')
+  const bin = FS.readFileSync(binPath, 'utf8').toString()
+  tokenAddress = prefixedAddressParamToByteCode(tokenAddress)
+  //add tokenaddress for param to the end of the bytecode
+  return bin + tokenAddress
+}
+
+function getPriceOracleBin(postageStampAddress) {
+  const binPath = Path.join(__dirname, '..', 'contracts', 'PriceOracle.bytecode')
+  const bin = FS.readFileSync(binPath, 'utf8').toString()
+  postageStampAddress = prefixedAddressParamToByteCode(postageStampAddress)
+  //add postageStampAddress for param to the end of the bytecode
+  return bin + postageStampAddress
+}
+
+/** Returns back contract hash */
+async function createContract(contractName, data, creatorAccount) {
   const transaction = await web3.eth.sendTransaction({
-    data: getSimpleSwapFactoryBin(erc20ContractAddress),
+    data: data,
     gasLimit: 6721975,
     gasPrice: web3.utils.toWei('10', 'gwei'),
     from: creatorAccount
   })
 
   if(!transaction.status) {
-    console.error('SimpleSwapFactory contract creation Error', error)
-    throw new Error('Error happened at creating SimpleSwapFactory contract creation')
+    console.error(`${contractName} contract creation Error`, error)
+    throw new Error(`Error happened at creating ${contractName} contract creation`)
   }
-  console.log(`SimpleSwapFactory contract creation was successful!\n`
+  console.log(`${contractName} contract creation was successful!\n`
     + `\tTransaction ID: ${transaction.transactionHash}\n`
     + `\tContract ID: ${transaction.contractAddress}`)
+  
+  return transaction.contractAddress
+}
+
+async function createSimpleSwapFactoryContract(erc20ContractAddress, creatorAccount) {
+  return createContract('SimpleSwapFactory', getSimpleSwapFactoryBin(erc20ContractAddress), creatorAccount)
+}
+
+async function createPostageStampContract(erc20ContractAddress, creatorAccount) {
+  return createContract('PostageStamp', getPostageStampBin(erc20ContractAddress), creatorAccount)
+}
+
+async function createPriceOracleContract(postageStampContractAddress, creatorAccount) {
+  return createContract('PriceOracle', getPriceOracleBin(postageStampContractAddress), creatorAccount)
 }
 
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(ERC20PresetMinterPauser, "Swarm Token", "BZZ").then(async () => {
     await createSimpleSwapFactoryContract(ERC20PresetMinterPauser.address, accounts[0])
+    const postageStampAddress = await createPostageStampContract(ERC20PresetMinterPauser.address, accounts[0])
+    await createPriceOracleContract(postageStampAddress, accounts[0])
   });
 };
--- scripts/bee.sh
+++ scripts/bee.sh
@@ -85,6 +85,8 @@ QUEEN_BOOTNODE=""
 PORT_MAPS=2
 SWAP=true
 SWAP_FACTORY_ADDRESS="0x5b1869D9A4C187F2EAa108f3062412ecf0526b24"
+POSTAGE_STAMP_ADDRESS="0xCfEB869F69431e42cdB54A4F4f105C19C080A601"
+PRICE_ORACLE_ADDRESS="0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B"
 INIT_ROOT_DATA_DIR="$MY_PATH/bee-data-dirs"
 
 # Decide script action
@@ -177,6 +179,8 @@ if [ -z "$QUEEN_CONTAINER_IN_DOCKER" ] || $EPHEMERAL ; then
         --swap-enable=$SWAP \
         --swap-endpoint="http://$SWARM_BLOCKCHAIN_NAME:9545" \
         --swap-factory-address=$SWAP_FACTORY_ADDRESS \
+        --postage-stamp-address=$POSTAGE_STAMP_ADDRESS \
+        --price-oracle-address=$PRICE_ORACLE_ADDRESS \
         --welcome-message="You have found the queen of the beehive..." \
         --cors-allowed-origins="*"
 else
@@ -221,6 +225,8 @@ for i in $(seq 1 1 "$WORKERS"); do
           --swap-enable=$SWAP \
           --swap-endpoint="http://$SWARM_BLOCKCHAIN_NAME:9545" \
           --swap-factory-address=$SWAP_FACTORY_ADDRESS \
+          --postage-stamp-address=$POSTAGE_STAMP_ADDRESS \
+          --price-oracle-address=$PRICE_ORACLE_ADDRESS \
           --welcome-message="I'm just Bee worker ${i} in the beehive." \
           --cors-allowed-origins="*"
   else
