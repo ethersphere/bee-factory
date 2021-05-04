0819569067ffffdda12c02a8b70bf97bba70240a
nugaon <toth.viktor.levente@gmail.com>
2021-05-04 15:31:31 +0200

    ci: export artitary set variables on docker push

--- scripts/publish-environment.sh
+++ scripts/publish-environment.sh
@@ -5,8 +5,9 @@ MY_PATH=$( cd "$MY_PATH" && pwd )
 BEE_IMAGE_PREFIX=$("$MY_PATH/utils/env-variable-value.sh" BEE_IMAGE_PREFIX)
 BEE_ENV_PREFIX=$("$MY_PATH/utils/env-variable-value.sh" BEE_ENV_PREFIX)
 BEE_VERSION=$("$MY_PATH/utils/env-variable-value.sh" BEE_VERSION)
-
+echo "Search Docker built images with the following parameters: $BEE_IMAGE_PREFIX/$BEE_ENV_PREFIX*:$BEE_VERSION"
 DOCKER_IMAGES=$(docker image ls -qaf reference="$BEE_IMAGE_PREFIX/$BEE_ENV_PREFIX*:$BEE_VERSION")
+echo "Push docker images: $DOCKER_IMAGES"
 for DOCKER_IMAGE in $DOCKER_IMAGES
 do
   echo "$DOCKER_IMAGE"
