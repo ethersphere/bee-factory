b3fe994fe07ed92297cc5371be2337f516d57661
nugaon <toth.viktor.levente@gmail.com>
2021-05-04 15:22:57 +0200

    fix: pull env variables from .env file in publish-environment.sh

--- scripts/publish-environment.sh
+++ scripts/publish-environment.sh
@@ -2,6 +2,10 @@
 MY_PATH=$(dirname "$0")
 MY_PATH=$( cd "$MY_PATH" && pwd )
 
+BEE_IMAGE_PREFIX=$("$MY_PATH/utils/env-variable-value.sh" BEE_IMAGE_PREFIX)
+BEE_ENV_PREFIX=$("$MY_PATH/utils/env-variable-value.sh" BEE_ENV_PREFIX)
+BEE_VERSION=$("$MY_PATH/utils/env-variable-value.sh" BEE_VERSION)
+
 DOCKER_IMAGES=$(docker image ls -qaf reference="$BEE_IMAGE_PREFIX/$BEE_ENV_PREFIX*:$BEE_VERSION")
 for DOCKER_IMAGE in $DOCKER_IMAGES
 do
