7a8f38d49b2202f5ddcaae2e0cdd0be8f383cb05
Adam Uhlíř <adam@uhlir.dev>
2022-05-06 12:20:39 +0200

    fix: commit removing regex honors rc suffix

--- src/utils/config-sources.ts
+++ src/utils/config-sources.ts
@@ -9,7 +9,7 @@ export function stripCommit(version: string): string {
   }
 
   // If the version contains commit ==> hash remove it
-  return version.replace(/(-\w+)+$/g, '')
+  return version.replace('-stateful', '').replace(/-[0-9a-fA-F]{8}$/, '')
 }
 
 async function searchPackageJson(): Promise<string | undefined> {
