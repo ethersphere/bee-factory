31332f84ceefaa520d510b95c6b8d074979d7928
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-05-15 11:17:01 +0200

    feat: update traffic generator logic

--- src/services/traffic_generator.ts
+++ src/services/traffic_generator.ts
@@ -6,22 +6,23 @@ const DEFAULT_CHEQUE_COUNT_TARGET = 10;
 export async function generateTraffic(chequeCountTarget: number | undefined): Promise<void> {
   console.info('\nGenerating traffic...');
   const target = chequeCountTarget ?? DEFAULT_CHEQUE_COUNT_TARGET;
-  const bee = new Bee(`http://localhost:${BEE_NODES[1].apiPort}`);
+  const uploaderBee = new Bee(`http://localhost:${BEE_NODES[1].apiPort}`);
+  const chequeMonitorBee = new Bee(`http://localhost:${BEE_NODES[2].apiPort}`);
 
   console.info('\nBuying storage...');
-  const batchId = await bee.buyStorage(Size.fromMegabytes(100), Duration.fromWeeks(2));
-
-  await bee.depositBZZToChequebook(BZZ.fromDecimalString('10'));
+  const batchId = await uploaderBee.buyStorage(Size.fromGigabytes(1), Duration.fromWeeks(2));
 
   while (true) {
-    const claimableChequeCount = await getClaimableChequeNumber(bee);
+    const claimableChequeCount = await getClaimableChequeNumber(chequeMonitorBee);
     if (claimableChequeCount >= target) {
       console.info(`Queen node has at least ${target} cheques, stopping traffic generation.`);
       break;
     }
 
-    await uploadRandomData(bee, batchId);
-    await bee.uploadData(batchId, crypto.getRandomValues(new Uint8Array(4096)));
+    console.info('Uploading data ...')
+    for (let i = 0; i < 1000; i++) {
+      await uploadRandomData(uploaderBee, batchId);
+    }
   }
 }
 
@@ -40,6 +41,5 @@ async function getClaimableChequeNumber(bee: Bee): Promise<number> {
 }
 
 async function uploadRandomData(bee: Bee, batchId: BatchId): Promise<void> {
-  console.info('\nUpload random data...');
   await bee.uploadData(batchId, crypto.getRandomValues(new Uint8Array(4096)));
 }
